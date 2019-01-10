dfdac002d822e758cb6ca95e7f234f9b9615866a
James Fleming <james@electronic-quill.net>
2019-01-10 21:45:22 +0100

    Decruft shell.nix

--- shell.nix
+++ shell.nix
@@ -7,24 +7,10 @@ stdenv.mkDerivation rec {
         # Neo4j
         pkgs.neo4j
         # Lisp env
-        #pkgs.gcc_multi
-        #pkgs.gcc
-        #pkgs.libyaml
-        #pkgs.openssl
         pkgs.sbcl
-        # Python env
-        #pkgs.python36Packages.requests
-        #pkgs.python36Packages.pylint
-        #pkgs.python3
     ];
 
-    env = buildEnv { name = name; paths = buildInputs;
-    };
-
-    #LD_LIBRARY_PATH = stdenv.lib.makeLibraryPath [
-    #    pkgs.openssl
-    #    pkgs.libyaml
-    #];
+    env = buildEnv { name = name; paths = buildInputs; };
 
     shellHook = "export PS1='\n\\[\\033[01;32m\\][nix cl-webcat] \\w\\$\\[\\033[00m\\] '";
 
