dd17d11561d1bc0258f6a6613a7caf1e8bbde2cc
James Fleming <james@electronic-quill.net>
2020-10-12 18:41:30 +0200

    Replace openssl with libressl

--- shell.nix
+++ shell.nix
@@ -5,13 +5,18 @@ stdenv.mkDerivation rec {
 
     buildInputs = [
         # Neo4j
-        pkgs.neo4j
+        #pkgs.neo4j
         # Lisp env
         pkgs.sbcl
+        # SSL
+        pkgs.libressl
     ];
 
     env = buildEnv { name = name; paths = buildInputs; };
 
-    shellHook = "export PS1='\n\\[\\033[01;32m\\][nix cl-webcat] \\w\\$\\[\\033[00m\\] '";
+    LD_LIBRARY_PATH = stdenv.lib.makeLibraryPath [
+        pkgs.libressl
+    ];
 
+    shellHook = "export PS1='\n\\[\\033[01;32m\\][nix cl-webcat] \\w\\$\\[\\033[00m\\] '";
 }
