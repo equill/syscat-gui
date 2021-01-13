7551bef482d031bf02df43423781649fca67095c
James Fleming <james@electronic-quill.net>
2021-01-13 16:26:54 +0100

    Bugfix addition of templates
    Copy _all_ template files into the Docker image, including the HTML
    ones.

--- scripts/docker/nix-builder.sh
+++ scripts/docker/nix-builder.sh
@@ -2,7 +2,7 @@ export PATH="$coreutils/bin"
 mkdir -p $out/bin
 cp $clwebcatpath $out/bin/clwebcat
 mkdir -p $out/templates
-cp $templatepath/*.tmpl $out/templates/
+cp $templatepath/* $out/templates/
 mkdir -p $out/static/js
 mkdir $out/static/css
 cp -r $staticpath/js/*js $out/static/js/
